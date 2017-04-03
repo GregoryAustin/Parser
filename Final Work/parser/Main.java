4801cbed584a6d67e80dc7b676ca3ad1732cc5ca
RavenBlood7 <juan.dupreez82@gmail.com>
2017-04-03 17:59:43 +0200

    Added a lexer to our parser. yay

---
+++ Final Work/parser/Main.java
@@ -0,0 +1,28 @@
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//testing the new parser tokenList
+		/*TokenList tl = new TokenList("lexeroutput");
+		System.out.println(tl);
+		while (!tl.isEmpty()){
+			TokenNode temp = tl.removeFromHead();
+			System.out.println("Removed: " +  temp.tokenClass);
+		}
+		System.out.println(tl);
+	}*/ 
+		Parser prsr = new Parser();
+		prsr.parse();
+		System.out.println("__________Parser output_____________");
+		prsr.print();
+		prsr.prune();
+		System.out.println("__________Parser pruned output_____________");
+		prsr.print();
+		//Context cont = new Context();
+
+	}
+
+
+}
+
